43a822eb63c9a4804ac2570637f31bd01265f393
Дреняев Максим <drenyaev.max@gmail.com>
2022-04-11 16:53:29 +0300

    b144

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,11 +1,12 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
  private:
-    T arr[size];
+    T arr[10];
     int first, last;
 
  public:
